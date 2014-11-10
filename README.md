ee0434c78667c142f04bf876090f31496edd78ba
Open-Steps.org <contact@open-steps.org>
2014-11-10 17:12:38 +0100

    First commit with plugin structure definition

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
 wp-ckan
 =======
 
-ODM Project is composed by 2 main components: 1. A centralized database implemented with CKAN 2. A multi-site multilingual front-end implemented with Wordpress In order to integrate both components and facilitate its interoperability a Wordpress plugin providing mechanisms for that purpose will be developed.
+A wordpress plugin for integrating CKAN and WP.
+
+## Description
+
+wp-ckan is a wordpress plugin that exposes a series of functionalities to bring content stored in CKAN to Wordpress' UI and also provide mechanisms for archiving content generated on Wordpress into a CKAN instance.
+
+## Features
+
+### Feature 1: Add related CKAN datasets to posts.
+
+Plugin presents a Metabox while editing Posts with an autocompletion input field that
+allows the user to add related CKAN datasets. Related datasets are rendered through a
+live preview of the contents and metadata included in the dataset (title, description, and
+resources)
+
+Add an option on the settings for the user to choose if the previews of the related datasets
+are shown automatically or not and to configure how they will be shown. Alternatively,
+users can use shortcodes or template tags for embedding information about related
+datasets on other parts of the posts or pages.
+
+Template Tags:
+```php
+<?php wp_ckan_get_related_datasets('post_id','preview'); ?>
+```
+Shortcodes:
+```php
+[wp_ckan_related_datasets show_preview=”yes” ]
+```
+
+### Feature 2: Query lists of CKAN datasets
+
+Plugin will expose a function that returns a list of CKAN datasets resulting after querying
+CKAN's API. Datasets can be queried after Organization, Group and/or specifying a textual
+search.
+The results of this function can be shown anywhere on a Wordpress instance (Posts,
+Pages, etc..) by calling a shortcode or template tag. This function could be integrated
+within the search mechanism of the chosen Wordpress theme (JEO) in order to present
+both contents from WP and CKAN together (i.e 2 column result list)
+
+Template Tags:
+```php
+<?php wp_ckan_query_datasets('query','limit'); ?>
+<?php wp_ckan_query_datasets_from_organization('query','organization','limit'); ?>
+<?php wp_ckan_query_datasets_from_group('query','group','limit'); ?>
+```
+Shortcodes:
+```php
+[wp_ckan_query_datasets query=”forestry” organization=”odmcambodia” group=”news”
+limit=”5”]
+```
+
+Returns a list of datasets found after querying the CKAN database. 4 possible parameters:
+1. query: a text to search forestry.
+2. organization: filter datasets that belong to specified organization.
+3. group: filter datasets that are assigned to specified group
+4. limit: limit the number of results
+
+### Feature 3: Archiving WP Posts in CKAN
+
+The plugin will expose a function that archives WP-generated content into CKAN. This function
+can be configured to be triggered when a post is published/modified or periodically (hourly, daily,
+weekly). For instance, using Hooks to trigger this function when a Wordpress' post is created,
+modified or deleted (See List of Wordpress Hooks).
+In order to be able to identify and relate Wordpress' content (Posts) with archived content in CKAN,
+Wordpress' unique id identifier (UUID) will be used.
+
+## Installation
+
+## License
---
+++ wp-ckan.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Plugin Name: wp-ckan
+ * Plugin URI:
+ * Description: wp-ckan is a wordpress plugin that exposes a series of functionalities to bring content stored in CKAN to Wordpress' UI and also provide mechanisms for archiving content generated on Wordpress into a CKAN instance.
+ * Version: 0.1
+ * Author: Alex Corbi
+ * Author URI: http://www.open-steps.org
+ * License: TBD
+ */
+
+<?php
+if(!class_exists('wp-ckan'))
+{
+    class wp-ckan
+    {
+        /**
+         * Construct the plugin object
+         */
+        public function __construct()
+        {
+            // register actions
+            add_action('admin_init', array(&$this, 'admin_init'));
+            add_action('admin_menu', array(&$this, 'add_menu'));
+        } // END public function __construct
+
+        /**
+         * Activate the plugin
+         */
+        public static function activate()
+        {
+            // Do nothing
+        } // END public static function activate
+
+        /**
+         * Deactivate the plugin
+         */
+        public static function deactivate()
+        {
+            // Do nothing
+        } // END public static function deactivate
+
+        // Installation and uninstallation hooks
+        register_activation_hook(__FILE__, array('wp-ckan', 'activate'));
+        register_deactivation_hook(__FILE__, array('wp-ckan', 'deactivate'));
+
+        // instantiate the plugin class
+        $wp-ckan = new wp-ckan();
+
+        // Add a link to the settings page onto the plugin page
+        if(isset($wp-ckan))
+        {
+            // Add the settings link to the plugins page
+            function plugin_settings_link($links)
+            {
+                $settings_link = '<a href="options-general.php?page=wp-ckan">Settings</a>';
+                array_unshift($links, $settings_link);
+                return $links;
+            }
+
+            $plugin = plugin_basename(__FILE__);
+            add_filter("plugin_action_links_$plugin", 'plugin_settings_link');
+        }
+
+
+        /**
+         * hook into WP's admin_init action hook
+         */
+        public function admin_init()
+        {
+            // Set up the settings for this plugin
+            $this->init_settings();
+            // Possibly do additional admin_init tasks
+        }
+
+        /**
+         * Initialize some custom settings
+         */
+        public function init_settings()
+        {
+            // register the settings for this plugin
+            register_setting('wp-ckan-group', 'setting_a');
+            register_setting('wp-ckan-group', 'setting_b');
+        }
+
+        /**
+         * add a menu
+         */
+        public function add_menu()
+        {
+            add_options_page('WP-CKAN Settings', 'WP-CKAN', 'manage_options', 'wp-ckan', array(&$this, 'plugin_settings_page'));
+        } // END public function add_menu()
+
+        /**
+         * Menu Callback
+         */
+        public function plugin_settings_page()
+        {
+            if(!current_user_can('manage_options'))
+            {
+                wp_die(__('You do not have sufficient permissions to access this page.'));
+            }
+
+            // Render the settings template
+            include(sprintf("%s/templates/settings.php", dirname(__FILE__)));
+        }
+
+    } // END class WP-CKAN-Plugin
+} // END if(!class_exists('WP-CKAN-Plugin'))
