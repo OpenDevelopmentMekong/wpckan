25d0e695e1e5ca1af913388183f557b92f373794
Open-Steps.org <contact@open-steps.org>
2014-12-03 23:28:49 +0100

    Implementing #4, added template for related dataset metabox

---
+++ templates/related_datasets_metabox.php
@@ -0,0 +1,6 @@
+<?php if (is_null($related_dataset)) die(); ?>
+
+<label for="wpckan_dataset_url_field">
+  <?php _e( 'Dataset\'s URL', 'myplugin_textdomain' ) ?>
+</label>
+<input onChange="wpckan_related_dataset_metabox_on_change();" type="text" id="wpckan_dataset_url_field" name="wpckan_dataset_url_field" value="<?php esc_attr( $related_dataset ) ?>" size="25" />
