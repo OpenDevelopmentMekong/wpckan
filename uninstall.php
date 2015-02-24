943a53759f2eb98d3ed56292f95153632728af1e
Open-Steps.org <contact@open-steps.org>
2015-02-24 12:25:05 +0100

    Fixes #66 Improved uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -3,39 +3,27 @@
  if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
   exit;
 
- if ( get_option( 'setting_ckan_url' ) != false ) {
-  delete_option( 'setting_ckan_url' );
- }
-
  if ( get_option( 'setting_ckan_api' ) != false ) {
   delete_option( 'setting_ckan_api' );
  }
 
- if ( get_option( 'setting_archive_freq' ) != false ) {
-  delete_option( 'setting_archive_freq' );
+ if ( get_option( 'setting_ckan_url' ) != false ) {
+  delete_option( 'setting_ckan_url' );
  }
 
- if ( get_option( 'setting_ckan_organization' ) != false ) {
-  delete_option( 'setting_ckan_organization' );
- }
-
- if ( get_option( 'setting_ckan_group' ) != false ) {
-  delete_option( 'setting_ckan_group' );
- }
-
- if ( get_option( 'setting_ckan_valid_settings_read' ) != false ) {
-  delete_option( 'setting_ckan_valid_settings_read' );
- }
-
- if ( get_option( 'setting_ckan_valid_settings_write' ) != false ) {
-  delete_option( 'setting_ckan_valid_settings_write' );
+ if ( get_option( 'setting_log_enabled' ) != false ) {
+  delete_option( 'setting_log_enabled' );
  }
 
  if ( get_option( 'setting_log_path' ) != false ) {
   delete_option( 'setting_log_path' );
  }
 
- if ( get_option( 'setting_log_enabled' ) != false ) {
-  delete_option( 'setting_log_enabled' );
+ foreach (get_post_types() as $post_type) {
+  $option_name = "setting_supported_post_types_" . $post_type;
+  if ( get_option( $option_name ) != false ) {
+   delete_option( $option_name );
+  }
  }
+
 ?>
