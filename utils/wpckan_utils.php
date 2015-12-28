fbbe10fdb4df62b3aecd1ef660beed4f6711e568
chris-aeviator <christopher@aeviator.cc>
2015-12-28 18:50:29 +0100

    added format switch to dataset list

--- utils/wpckan_utils.php
+++ utils/wpckan_utils.php
@@ -283,8 +283,16 @@
 
     if ((count($dataset_array) == 0) && $blank_on_empty)
       return "";
-
-    return wpckan_output_template( plugin_dir_path( __FILE__ ) . '../templates/dataset_list.php',$filtered_dataset_array,$atts);
+    // user can chose to output pure data instead of HTML representation with including "format" shortcode
+    if (array_key_exists("format",$atts)){
+      // json template
+      if ($atts["format"]=="json") {
+        return wpckan_output_template( plugin_dir_path( __FILE__ ) . '../templates/dataset_list_format_json.php',$filtered_dataset_array,$atts);
+      }
+    }
+    else{
+      return wpckan_output_template( plugin_dir_path( __FILE__ ) . '../templates/dataset_list.php',$filtered_dataset_array,$atts);
+    }
   }
 
   /*
