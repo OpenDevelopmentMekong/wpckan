0007b515ed8a92c93076a870da6a4051861a6b6c
chris-aeviator <christopher@aeviator.cc>
2015-12-29 14:14:09 +0100

    added format json option with sample json

---
+++ templates/dataset_list_format_json.php
@@ -0,0 +1,189 @@
+<?php
+$json_string='[
+{
+name: "Land",
+titles: {
+en: "Land",
+km: "ដី",
+th: "ที่ดิน",
+vi: "Đất"
+},
+children: [
+{
+name: "Agriculture and fishing",
+titles: {
+en: "Agriculture and fishing",
+km: "កសិកម្ម និងការនេសាទ",
+th: "การเกษตรและการประมง",
+vi: "Nông nghiệp và thủy sản"
+}
+},
+{
+name: "Disasters and emergency response",
+titles: {
+en: "Disasters and emergency response",
+km: "គ្រោះមហន្តរាយ និងការឆ្លើយតបក្នុងគ្រាអាសន្ន",
+th: "ภัยพิบัติและการรับมือสถานการณ์ฉุกเฉิน",
+vi: "Thảm họa và ứng phó khẩn cấp"
+}
+},
+{
+name: "Environment and natural resources",
+titles: {
+en: "Environment and natural resources",
+km: "បរិស្ថាន និងធនធានធម្មជាតិ",
+th: "ทรัพยากรธรรมชาติและสิ่งแวดล้อม",
+vi: "Môi trường và tài nguyên thiên nhiên"
+}
+},
+{
+name: "Extractive industries",
+titles: {
+en: "Extractive industries",
+km: "ឧស្សាហកម្មនិស្សារណកម្ម",
+th: "อุตสาหกรรมสาร",
+vi: "Ngành công nghiệp khai thác"
+}
+},
+{
+name: "Land",
+titles: {
+en: "Land",
+km: "ដី",
+th: "ที่ดิน",
+vi: "Đất"
+},
+class: "tooltip"
+}
+]
+},
+{
+name: "Economy",
+titles: {
+en: "Economy",
+km: "Economy",
+th: "Economy",
+vi: "Economy"
+},
+children: [
+{
+name: "Economy and commerce",
+titles: {
+en: "Economy and commerce",
+km: "សេដ្ឋកិច្ច និងពាណិជ្ជកម្ម ",
+th: "เศรษฐกิจและการพานิชย์",
+vi: "Kinh tế và thương mại"
+}
+},
+{
+name: "Energy",
+titles: {
+en: "Energy",
+km: "ថាមពល",
+th: "พลังงาน",
+vi: "Năng lượng"
+}
+},
+{
+name: "Infrastructure",
+titles: {
+en: "Infrastructure",
+km: "ហេដ្ឋារចនាសម្ព័ន្ធ",
+th: "โครงสร้าง",
+vi: "Industry"
+},
+class: "tooltip"
+},
+{
+name: "Industry",
+titles: {
+en: "Industry",
+km: "Industry",
+th: "Industry",
+vi: "Industry"
+}
+},
+{
+name: "Labor",
+titles: {
+en: "Labor",
+km: "ពលកម្ម",
+th: "แรงงาน",
+vi: "Lao động"
+}
+}
+]
+},
+{
+name: "People",
+titles: {
+en: "People",
+km: "People",
+th: "People",
+vi: "People"
+},
+children: [
+{
+name: "Aid and development",
+titles: {
+en: "Aid and development",
+km: "ជំនួយ និងការអភិវឌ្ឍ",
+th: "ความช่วยเหลือและการพัฒนา",
+vi: "Viện trợ và phát triển"
+}
+},
+{
+name: "Government",
+titles: {
+en: "Government",
+km: "រដ្ឋាភិបាល",
+th: "รัฐบาล",
+vi: "Chính phủ"
+},
+class: "tooltip"
+},
+{
+name: "Law and judiciary",
+titles: {
+en: "Law and judiciary",
+km: "ច្បាប់ និងប្រព័ន្ធតុលាការ",
+th: "กฎหมายและตุลาการ",
+vi: "Luật pháp và tòa án"
+},
+class: "tooltip"
+},
+{
+name: "Population and censuses",
+titles: {
+en: "Population and censuses",
+km: "ប្រជាជន និងជំរឿន",
+th: "ประชากรและสำมะโนประชากร",
+vi: "Dân số và thống kê"
+}
+},
+{
+name: "Social development",
+titles: {
+en: "Social development",
+km: "ការអភិវឌ្ឍសង្គម",
+th: "การพัฒนาสังคม",
+vi: "Phát triển xã hội"
+},
+class: "tooltip"
+},
+{
+name: "Urban administration and development",
+titles: {
+en: "Urban administration and development",
+km: "ការអភិវឌ្ឍ និងការគ្រប់គ្រងទីក្រុង",
+th: "การบริหารและการพัฒนาเมือง",
+vi: "Quản lý nhà nước về đô thị và phát triển đô thị"
+}
+}
+]
+}
+]';
+echo $json_string;
+
+
+?>
