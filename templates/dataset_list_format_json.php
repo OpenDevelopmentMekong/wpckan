0c1c2e408d17543b02d284a49cf4300d1b1de487
chris-aeviator <christopher@aeviator.cc>
2015-12-30 00:06:26 +0100

    added json representation of dataset list

--- templates/dataset_list_format_json.php
+++ templates/dataset_list_format_json.php
@@ -1,189 +1,120 @@
+<?php if (is_null($data)) die(); ?>
+
 <?php
-$json_string='[
-{
-name: "Land",
-titles: {
-en: "Land",
-km: "ដី",
-th: "ที่ดิน",
-vi: "Đất"
-},
-children: [
-{
-name: "Agriculture and fishing",
-titles: {
-en: "Agriculture and fishing",
-km: "កសិកម្ម និងការនេសាទ",
-th: "การเกษตรและการประมง",
-vi: "Nông nghiệp và thủy sản"
-}
-},
-{
-name: "Disasters and emergency response",
-titles: {
-en: "Disasters and emergency response",
-km: "គ្រោះមហន្តរាយ និងការឆ្លើយតបក្នុងគ្រាអាសន្ន",
-th: "ภัยพิบัติและการรับมือสถานการณ์ฉุกเฉิน",
-vi: "Thảm họa và ứng phó khẩn cấp"
-}
-},
-{
-name: "Environment and natural resources",
-titles: {
-en: "Environment and natural resources",
-km: "បរិស្ថាន និងធនធានធម្មជាតិ",
-th: "ทรัพยากรธรรมชาติและสิ่งแวดล้อม",
-vi: "Môi trường và tài nguyên thiên nhiên"
-}
-},
-{
-name: "Extractive industries",
-titles: {
-en: "Extractive industries",
-km: "ឧស្សាហកម្មនិស្សារណកម្ម",
-th: "อุตสาหกรรมสาร",
-vi: "Ngành công nghiệp khai thác"
-}
-},
-{
-name: "Land",
-titles: {
-en: "Land",
-km: "ដី",
-th: "ที่ดิน",
-vi: "Đất"
-},
-class: "tooltip"
-}
-]
-},
-{
-name: "Economy",
-titles: {
-en: "Economy",
-km: "Economy",
-th: "Economy",
-vi: "Economy"
-},
-children: [
-{
-name: "Economy and commerce",
-titles: {
-en: "Economy and commerce",
-km: "សេដ្ឋកិច្ច និងពាណិជ្ជកម្ម ",
-th: "เศรษฐกิจและการพานิชย์",
-vi: "Kinh tế và thương mại"
-}
-},
-{
-name: "Energy",
-titles: {
-en: "Energy",
-km: "ថាមពល",
-th: "พลังงาน",
-vi: "Năng lượng"
-}
-},
-{
-name: "Infrastructure",
-titles: {
-en: "Infrastructure",
-km: "ហេដ្ឋារចនាសម្ព័ន្ធ",
-th: "โครงสร้าง",
-vi: "Industry"
-},
-class: "tooltip"
-},
-{
-name: "Industry",
-titles: {
-en: "Industry",
-km: "Industry",
-th: "Industry",
-vi: "Industry"
-}
-},
-{
-name: "Labor",
-titles: {
-en: "Labor",
-km: "ពលកម្ម",
-th: "แรงงาน",
-vi: "Lao động"
-}
-}
-]
-},
-{
-name: "People",
-titles: {
-en: "People",
-km: "People",
-th: "People",
-vi: "People"
-},
-children: [
-{
-name: "Aid and development",
-titles: {
-en: "Aid and development",
-km: "ជំនួយ និងការអភិវឌ្ឍ",
-th: "ความช่วยเหลือและการพัฒนา",
-vi: "Viện trợ và phát triển"
-}
-},
-{
-name: "Government",
-titles: {
-en: "Government",
-km: "រដ្ឋាភិបាល",
-th: "รัฐบาล",
-vi: "Chính phủ"
-},
-class: "tooltip"
-},
-{
-name: "Law and judiciary",
-titles: {
-en: "Law and judiciary",
-km: "ច្បាប់ និងប្រព័ន្ធតុលាការ",
-th: "กฎหมายและตุลาการ",
-vi: "Luật pháp và tòa án"
-},
-class: "tooltip"
-},
-{
-name: "Population and censuses",
-titles: {
-en: "Population and censuses",
-km: "ប្រជាជន និងជំរឿន",
-th: "ประชากรและสำมะโนประชากร",
-vi: "Dân số và thống kê"
-}
-},
-{
-name: "Social development",
-titles: {
-en: "Social development",
-km: "ការអភិវឌ្ឍសង្គម",
-th: "การพัฒนาสังคม",
-vi: "Phát triển xã hội"
-},
-class: "tooltip"
-},
-{
-name: "Urban administration and development",
-titles: {
-en: "Urban administration and development",
-km: "ការអភិវឌ្ឍ និងការគ្រប់គ្រងទីក្រុង",
-th: "การบริหารและการพัฒนาเมือง",
-vi: "Quản lý nhà nước về đô thị và phát triển đô thị"
-}
-}
-]
-}
-]';
-echo $json_string;
+  $include_fields_dataset = array();
+  array_push($include_fields_dataset,"title"); //ensure that this field is present
+  array_push($include_fields_dataset,"notes"); //ensure that this field is present
+  $include_fields_resources = array();
+  array_push($include_fields_resources,"name"); //ensure that this field is present
+  array_push($include_fields_resources,"description"); //ensure that this field is present
+  if (array_key_exists("include_fields_dataset",$atts))
+    $include_fields_dataset = explode(",",$atts["include_fields_dataset"]);
+  if (array_key_exists("include_fields_resources",$atts))
+    $include_fields_resources = explode(",",$atts["include_fields_resources"]);
+  if (array_key_exists("related_dataset",$atts)) $count = count($atts["related_dataset"]);
+  if (array_key_exists("count",$atts)) $count = $atts["count"];
+
+// field extras
+  $include_fields_extra = array();
+  if (array_key_exists("include_fields_extra",$atts))
+    $include_fields_extra = explode(",",$atts["include_fields_extra"]);
+  // insert type
+  $dataset_count=count($data);
 
 
 ?>
+<?php
+//  {
+//   "Herausgeber": "Xema",
+//   "Nummer": "1234-5678-9012-3456",
+//   "Deckung": 2e+6,
+//   "Waehrung": "EURO",
+//   "Inhaber": {
+//     "Name": "Mustermann",
+//     "Vorname": "Max",
+//     "maennlich": true,
+//     "Hobbys": [ "Reiten", "Golfen", "Lesen" ],
+//     "Alter": 42,
+//     "Kinder": [],
+//     "Partner": null
+//   }
+// }
+
+// $title=$dataset["title"];
+// $title_url=
+?>
+{
+    "wpckan_dataset_list":{
+      <?php foreach ($data as $index_data => $dataset){ ?>
+        <?php $resource_count=count($dataset["resources"]);?>
+        <!-- set variables -->
+        <?php
+          // wpckan_dataset_list
+           $title=$dataset["title"];
+           $title_url= wpckan_get_link_to_dataset($dataset["name"]);
+           $notes=$dataset["notes"];
+           $url=$dataset["url"];
+           $license=$dataset["license_id"];
+           $license_url=$dataset["license_url"];
+           $metadata_created=$dataset["metadata_created"];
+           $metadata_modified=$dataset["metadata_modified"];
+           $author=$dataset["author"];
+           $author_email=$dataset["author_email"];
+
+
+        ?>
+          <!-- set json -->
+          "<?php echo $title;?>":{
+            <?php if (array_key_exists("title",$dataset) && !wpckan_is_null_or_empty_string($dataset["title"]) && in_array("title",$include_fields_dataset)) {?>
+              "wpckan_dataset_title_url":"<?php echo $title_url;?>",
+            <?php }?>
+            "wpckan_dataset_notes":"<?php #echo $notes;?>",
+            "wpckan_dataset_url":"<?php echo $url;?>",
+            "wpckan_dataset_license":"<?php #echo $license;?>",
+            "wpckan_dataset_license_url":"<?php echo $license_url;?>",
+            "wpckan_dataset_metadata_created":"<?php echo $metadata_created;?>",
+            "wpckan_dataset_metadata_modified":"<?php echo $metadata_modified;?>",
+            "wpckan_dataset_author":"<?php echo $author;?>",
+            "wpckan_dataset_author_email":"<?php echo $author_email;?>",
+            "wpckan_resources_list":{
+              <?php foreach ($dataset["resources"] as $index =>$resource){
+                //  wpckan_resources_list
+                $resoure_name=$resource["name"];
+                $resource_name_link=wpckan_get_link_to_resource($dataset["name"],$resource["id"]);
+                $resource_description=$resource["description"];
+                if (array_key_exists("revision_timestamp",$resource) && !wpckan_is_null_or_empty_string($resource["revision_timestamp"]) && in_array("revision_timestamp",$include_fields_resources)) {
+                $resource_revision_timestamp=$resource["revision_timestamp"];
+                }
+                $resource_format=$resource["format"];
+                $resource_created=$resource["created"];
+                if (array_key_exists("created",$resource) && !wpckan_is_null_or_empty_string($resource["created"]) && in_array("created",$include_fields_resources)) {
+                  $resource_language=$resource["language"][0];
+                }
+                ?>
+
+                "<?php echo $resoure_name;?>":{
+                  "wpckan_resource_name_link":"<?php echo $resource_name_link;?>",
+                  "wpckan_resource_description":"",
+                  <?php if (array_key_exists("revision_timestamp",$resource) && !wpckan_is_null_or_empty_string($resource["revision_timestamp"]) && in_array("revision_timestamp",$include_fields_resources)) {?>
+                    "wpckan_resource_revision_timestamp":"<?php echo $resource_revision_timestamp;?>",
+                  <?php } ?>
+                  "wpckan_resource_format":"<?php echo $resource_format;?>",
+                  "wpckan_resource_created":"<?php echo $resource_created;?>",
+
+                    "wpckan_resource_language":"<?php echo $resource_language;?>"
+
+                }<?php if ($index == $resource_count - 1) { echo "";} else {echo ",";}?>
+
+              <?php } ?>
+            },
+            "wpckan_dataset_extras":{
+              <?php foreach ($include_fields_extra as $index_extra => $extra) {?>
+                <?php $extra_count=count($include_fields_extra);?>
+                "wpkan_dataset_extras-<?php echo $extra;?>":"<?php echo $dataset[$extra];?>"<?php if ($index_extra == $extra_count - 1) { echo "";} else {echo ",";}?>
+              <?php } ?>
+            }
+          }<?php if ($index_data == $dataset_count - 1) { echo "";} else {echo ",";}?>
+
+        <?php } ?>
+      }
+    }
