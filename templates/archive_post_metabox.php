c2399357538d6e8f7e4fb315907b99edeeea992c
Alex Corbi <a.corbi@gmail.com>
2016-05-12 14:37:22 +0200

    specified correct text_domain parameter

--- templates/archive_post_metabox.php
+++ templates/archive_post_metabox.php
@@ -16,32 +16,32 @@
         }
   ?>
 
-  <label for="wpckan_archive_post_orga"><b><?php _e('CKAN Organization','wpckan_setting_ckan_organization_title') ?></b></label>
+  <label for="wpckan_archive_post_orga"><b><?php _e('CKAN Organization','wpckan') ?></b></label>
   <p><select name="wpckan_archive_post_orga" id="wpckan_archive_post_orga">
-    <option <?php if($archive_orga == -1) echo 'selected="selected"' ?> value="-1"><?php _e('None','wpckan_archive_post_orga_none')?></option>
+    <option <?php if($archive_orga == -1) echo 'selected="selected"' ?> value="-1"><?php _e('None','wpckan')?></option>
     <?php foreach ($organization_list as $organization){ ?>
       <?php if (in_array($organization["display_name"],$organization_names)) { ?>
         <option <?php if($archive_orga == $organization["id"]) echo 'selected="selected"' ?> value="<?php echo $organization["id"] ?>"><?php echo $organization["display_name"]?></option>
       <?php } ?>
     <?php } ?>
   </select></p>
-  <label for="setting_ckan_group"><b><?php _e('CKAN Group','wpckan_setting_ckan_group_title') ?></b></label>
+  <label for="setting_ckan_group"><b><?php _e('CKAN Group','wpckan') ?></b></label>
   <p><select name="wpckan_archive_post_group" id="wpckan_archive_post_group">
-    <option <?php if($archive_group == -1) echo 'selected="selected"' ?> value="-1"><?php _e('None','wpckan_archive_post_group_none')?></option>
+    <option <?php if($archive_group == -1) echo 'selected="selected"' ?> value="-1"><?php _e('None','wpckan')?></option>
     <?php foreach ($group_list as $group){ ?>
       <?php if (in_array($group["display_name"],$group_names)) { ?>
         <option <?php if($archive_group == $group["id"]) echo 'selected="selected"' ?> value="<?php echo $group["id"] ?>"><?php echo $group["display_name"]?></option>
       <?php } ?>
     <?php } ?>
   </select></p>
-  <label for"wpckan_archive_post_freq"><b><?php _e('Archive contents when:','wpckan_archive_post_freq') ?></b></label>
+  <label for"wpckan_archive_post_freq"><b><?php _e('Archive contents when:','wpckan') ?></b></label>
   <p><select name="wpckan_archive_post_freq" id="wpckan_archive_post_freq">
-    <option value="0" <?php if($archive_freq == 0) echo 'selected="selected"' ?>><?php _e('Never archive','wpckan_archive_post_freq_0' )?></option>
-    <option value="1" <?php if($archive_freq == 1) echo 'selected="selected"' ?>><?php _e('Post is saved','wpckan_archive_post_freq_2') ?></option>
+    <option value="0" <?php if($archive_freq == 0) echo 'selected="selected"' ?>><?php _e('Never archive','wpckan' )?></option>
+    <option value="1" <?php if($archive_freq == 1) echo 'selected="selected"' ?>><?php _e('Post is saved','wpckan') ?></option>
   </select></p>
 
 <?php } else { ?>
 
-  <p class="error"><?php _e( 'wpckan is not correctly configured. Please, check the ', 'related_datasets_metabox_config_error' ) ?><a href="options-general.php?page=wpckan">Settings</a></p>
+  <p class="error"><?php _e( 'wpckan is not correctly configured. Please, check the ', 'wpckan' ) ?><a href="options-general.php?page=wpckan">Settings</a></p>
 
 <?php }?>
--- templates/settings.php
+++ templates/settings.php
@@ -19,40 +19,40 @@
         ?>
 
         <table class="form-table">
-          <th scope="row"><label><h3><?php _e('Connecting to CKAN','wpckan_settings_ckan_connection_header') ?></h3></label></th>
+          <th scope="row"><label><h3><?php _e('Connecting to CKAN','wpckan') ?></h3></label></th>
           <tr valign="top">
-              <th scope="row"><label for="setting_ckan_url"><?php _e('CKAN Url','wpckan_settings_ckan_url_title') ?></label></th>
+              <th scope="row"><label for="setting_ckan_url"><?php _e('CKAN Url','wpckan') ?></label></th>
               <td>
                 <input type="text" name="setting_ckan_url" id="setting_ckan_url" value="<?php echo $ckan_url ?>"/>
-                <p class="description"><?php _e('Specify protocol such as http:// or https://.','wpckan_settings_ckan_url_summary') ?>.</p>
+                <p class="description"><?php _e('Specify protocol such as http:// or https://.','wpckan') ?>.</p>
               </td>
           </tr>
           <tr valign="top">
-              <th scope="row"><label for="setting_ckan_api"><?php _e('CKAN Api key','wpckan_settings_ckan_api_title') ?></label></th>
+              <th scope="row"><label for="setting_ckan_api"><?php _e('CKAN Api key','wpckan') ?></label></th>
               <td>
                 <input type="text" name="setting_ckan_api" id="setting_ckan_api" value="<?php echo get_option('setting_ckan_api'); ?>"/>
-                <p class="description"><?php _e('Available under the profile page of a CKAN user with Admin rights.','wpckan_settings_ckan_api_summary') ?>.</p>
+                <p class="description"><?php _e('Available under the profile page of a CKAN user with Admin rights.','wpckan') ?>.</p>
               </td>
           </tr>
           <!-- Connection status -->
           <tr valign="top">
-            <th scope="row"><label><?php _e('Connection status','wpckan_settings_valid_connection_title') ?></label></th>
+            <th scope="row"><label><?php _e('Connection status','wpckan') ?></label></th>
             <td>
               <?php if ($valid_connection_read){ ?>
-                <p class="ok"><?php _e('CKAN URL specified correctly.','wpckan_settings_valid_connection_read_ok') ?></p>
+                <p class="ok"><?php _e('CKAN URL specified correctly.','wpckan') ?></p>
               <?php } else { ?>
-                <p class="error"><?php _e('Problem connecting to CKAN instance. Please, check the specified URL.','wpckan_settings_valid_connection_read_error') ?></p>
+                <p class="error"><?php _e('Problem connecting to CKAN instance. Please, check the specified URL.','wpckan') ?></p>
               <?php } ?>
               <?php if ($valid_connection_write){ ?>
-                <p class="ok"><?php _e('CKAN API Key specified correctly.','wpckan_settings_valid_connection_write_ok') ?></p>
+                <p class="ok"><?php _e('CKAN API Key specified correctly.','wpckan') ?></p>
               <?php } else { ?>
-                <p class="error"><?php _e('Please, specify a valid CKAN API Key.','wpckan_settings_valid_connection_write_error') ?></p>
+                <p class="error"><?php _e('Please, specify a valid CKAN API Key.','wpckan') ?></p>
               <?php } ?>
             </td>
           </tr>
           <!-- Related datasets -->
           <tr valign="top">
-            <th scope="row"><label for="settings_supported_post_types"><?php _e('Supported post types','wpckan_settings_supported_post_types_title') ?></label></th>
+            <th scope="row"><label for="settings_supported_post_types"><?php _e('Supported post types','wpckan') ?></label></th>
             <td>
              <?php
               foreach (get_post_types() as $post_type) {
@@ -63,18 +63,18 @@
            </td>
           </tr>
           <!-- Logging -->
-          <th scope="row"><label><h3><?php _e('Logging','wpckan_settings_logging_header') ?></h3></label></th>
+          <th scope="row"><label><h3><?php _e('Logging','wpckan') ?></h3></label></th>
           <tr valign="top">
-            <th scope="row"><label for="setting_log_enabled"><?php _e('Enable log','wpckan_settings_log_enabled_title') ?></label></th>
+            <th scope="row"><label for="setting_log_enabled"><?php _e('Enable log','wpckan') ?></label></th>
             <td>
               <input type="checkbox" name="setting_log_enabled" id="setting_log_enabled" <?php if ($logging_enabled)  echo 'checked="true"'; ?>/>
             </td>
           </tr>
           <tr valign="top">
-            <th scope="row"><label for="setting_log_path"><?php _e('Path','wpckan_settings_log_path_title') ?></label></th>
+            <th scope="row"><label for="setting_log_path"><?php _e('Path','wpckan') ?></label></th>
             <td>
               <input type="text" name="setting_log_path" id="setting_log_path" value="<?php echo $logging_path ?>"/>
-              <p class="description"><?php _e('Path where logs are going to be stored. Mind permissions.','wpckan_settings_ckan_logging_path_summary') ?></p>
+              <p class="description"><?php _e('Path where logs are going to be stored. Mind permissions.','wpckan') ?></p>
             </td>
           </tr>
         </table>
