22e471e40b223506fe1f02a74a7e088d9a3ce446
Alex Corbi <a.corbi@gmail.com>
2016-05-30 21:06:56 +0200

    running composer install

--- _ci/install.sh
+++ _ci/install.sh
@@ -7,6 +7,9 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update -q
 sudo apt-get install -q -y ansible
 
+echo "Install php dependencies"
+composer install
+
 echo "Downloading and unzipping odm_automation"
 wget https://github.com/OpenDevelopmentMekong/odm-automation/archive/master.zip -O /tmp/odm_automation.zip
 unzip /tmp/odm_automation.zip -d /tmp/
