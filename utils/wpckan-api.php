bd1d6e095c8ef10b545d8362a6724a67eede682b
Alex Corbi <a.corbi@gmail.com>
2016-11-28 10:49:56 +0100

    Fixes #191

--- utils/wpckan-api.php
+++ utils/wpckan-api.php
@@ -40,7 +40,7 @@
 			$iteration = 1;
       if ($total_count - ($iteration * 1000) > 0):
 				$attrs["limit"] = 1000;
-				$attrs["page"] = $iteration;
+				$attrs["page"] = $iteration + 1;
 				$query = '?'.compose_solr_query_from_attrs($attrs);
 				$ckanapi_url = $ckan_domain.'/api/3/action/package_search'.$query;
 
