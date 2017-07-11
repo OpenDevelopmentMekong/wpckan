eb8f8be5e34031ced1fb142594f0f7799960775e
Alex Corbi <a.corbi@gmail.com>
2017-07-11 18:18:27 +0200

    Fixes #315

--- js/metadata_dropdown.js
+++ js/metadata_dropdown.js
@@ -1,30 +1,8 @@
-function DropDown(el) {
-	this.dd = el;
-	this.initEvents();
-}
-
-DropDown.prototype = {
-		initEvents : function() {
-				var obj = this;
-
-				obj.dd.on('click', function(event){
-						$(this).toggleClass('active');
-						event.stopPropagation();
-				});
-		}
-}
-
 jQuery( document ).ready(function() {
 
 	$(".metadata-dropdown").on('click', function(event){
-		$(this).toggleClass('active');
-		return false;
-	});
-
-	var dd = new DropDown( $('.metadata-dropdown') );
-
-	$(document).click(function() {
-		$('.metadata-dropdown').removeClass('active');
+			$(this).toggleClass('active');
+			event.stopPropagation();
 	});
 
 });
