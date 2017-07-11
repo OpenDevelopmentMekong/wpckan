d430e096a8f64cf28a9e33e7108211312527e191
Alex Corbi <a.corbi@gmail.com>
2017-07-11 18:37:23 +0200

    Fixed typo

--- wpckan.php
+++ wpckan.php
@@ -31,7 +31,7 @@ if (!class_exists('wpckan')) {
     class wpckan
     {
         /**
-         * Construct the plugimargin-bottom: 20px;n object.
+         * Construct the plugin object.
          */
         public function __construct()
         {
