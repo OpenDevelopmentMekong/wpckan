d3e3962dda763d22a5fc5b8976324e26dd487154
Alex Corbi <a.corbi@gmail.com>
2017-10-13 12:36:12 +0200

    added wpckan_get_organization_for_user

--- utils/wpckan-utils.php
+++ utils/wpckan-utils.php
@@ -519,6 +519,16 @@
     return $group_names;
   }
 
+  function wpckan_get_organization_for_user(){
+    $organizations = array();
+    try{
+      $organizations = wpckan_api_get_organization_list_for_user();
+    }catch(Exception $e){
+      wpckan_log($e->getMessage());
+    }
+    return $organizations;
+  }
+
   function wpckan_get_organization_names_for_user(){
     $organizations = array();
     $organization_names = array();
