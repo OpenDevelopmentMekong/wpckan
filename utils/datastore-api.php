d74bdc908ebb954769e4a4cde41d11d23cf4ae86
root <root@opendevelopmentmekong.net>
2018-08-07 12:47:05 +0000

    initial altered ckan api limits

--- utils/datastore-api.php
+++ utils/datastore-api.php
@@ -27,7 +27,7 @@
 
  function wpckan_get_datastore_resources_filter($ckan_domain, $resource_id, $key, $value)
  {
-     $datastore_url = $ckan_domain.'/api/3/action/datastore_search?resource_id='.$resource_id.'&limit=9999&filters={"'.$key.'":"'.$value.'"}';
+     $datastore_url = $ckan_domain.'/api/3/action/datastore_search?resource_id='.$resource_id.'&limit=50&filters={"'.$key.'":"'.$value.'"}';
      $json = wpckan_get_or_cache($datastore_url,$resource_id. $key . $value);
 
      if ($json === false) {
@@ -44,7 +44,7 @@
 
  function wpckan_get_datastore_resource($ckan_domain, $resource_id)
  {
-     $datastore_url = $ckan_domain.'/api/3/action/datastore_search?resource_id='.$resource_id.'&limit=9999';
+     $datastore_url = $ckan_domain.'/api/3/action/datastore_search?resource_id='.$resource_id.'&limit=50';
      $json = wpckan_get_or_cache($datastore_url,$resource_id);
 
      if ($json === false) {
