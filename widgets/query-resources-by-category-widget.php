13d71e3c3814868f2ac994958a357f89dcae4144
S-mardii <s.mardy009@gmail.com>
2022-05-17 15:54:32 +0700

    [IMP] Add "More..." to Related resources widget

--- widgets/query-resources-by-category-widget.php
+++ widgets/query-resources-by-category-widget.php
@@ -9,7 +9,7 @@ class Wpckan_Query_Resources_By_Topic_Widget extends WP_Widget {
 		// widget actual processes
 		parent::__construct(
 			'wpckan_query_resources_by_topic_widget',
-			__('WPCKAN Query resources by post\'s category', 'wpckan'),
+			__('WPCKAN Related resources by categories', 'wpckan'),
 			array('description' => __('Queries CKAN for datasets with the post\'s category as value for the field specified.', 'wpckan'))
 		);
 
@@ -74,7 +74,7 @@ class Wpckan_Query_Resources_By_Topic_Widget extends WP_Widget {
 	public function widget( $args, $instance ) {
 		global $post;
 
-		$search_field = isset($instance['search_field']) ? $instance['search_field'] : 'title';
+		$search_field = isset($instance['search_field']) ? sanitize_text_field($instance['search_field']) : 'taxonomy';
 		$limit = isset($instance['limit']) ? $instance['limit'] : -1;
 		$categories_names = $this->get_categories($post);
 		$output_fields = isset($instance['output_fields']) ? $instance['output_fields'] : 'title';
@@ -120,6 +120,10 @@ class Wpckan_Query_Resources_By_Topic_Widget extends WP_Widget {
 
 				echo $output;
 
+				if (!empty($instance['more_link']) && $instance['more_link'] != "") :
+                    echo '<div style="text-align:right"><a href="' . $instance['more_link'] . '" target="_blank">' . $instance['more_text'] . '</a></div>';
+                endif;
+
 				echo $args['after_widget'];
 
 			endif;
@@ -135,12 +139,15 @@ class Wpckan_Query_Resources_By_Topic_Widget extends WP_Widget {
 	 */
 	public function form( $instance ) {
 
-		$title = !empty($instance['title']) ? __($instance['title'], 'wpckan') : __('Custom posts', 'wpckan');
+		$title = !empty($instance['title']) ? __(sanitize_text_field($instance['title']), 'wpckan') : __('Related resources by category', 'wpckan');
 		$type = !empty($instance['type']) ? $instance['type'] : 'dataset';
+		$search_field = !empty($instance['search_field']) ? sanitize_text_field($instance['search_field']) : 'taxonomy';
 		$organization = isset($instance['organization']) ? $instance['organization'] : -1;
 		$organization_list = [];
 		$sort = isset($instance['sort']) ? $instance['sort'] : 'metadata_modified+desc';
 		$template = isset($instance['template']) ? $instance['template'] : 'dataset-list';
+		$more_text = !empty($instance['more_text']) ? $instance['more_text'] : 'More...';
+        $more_link = !empty($instance['more_link']) ? $instance['more_link'] : '';
 
 		if (function_exists('wpckan_api_get_organizations_list')) {
 				try {
@@ -178,6 +185,14 @@ class Wpckan_Query_Resources_By_Topic_Widget extends WP_Widget {
 			<label for="<?php echo $this->get_field_id( 'limit' ); ?>"><?php _e( 'Select max number of posts to list (-1 to show all):' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id('limit');?>" name="<?php echo $this->get_field_name('limit');?>" type="number" value="<?php echo $limit;?>">
 		</p>
+		<p>
+            <label for="<?php echo $this->get_field_id('more_text'); ?>"><?php _e('More dataset: Link label', 'wpckan'); ?></label>
+            <input class="widefat" type="text" id="<?php echo $this->get_field_id('more_text'); ?>" name="<?php echo $this->get_field_name('more_text'); ?>" value="<?php echo esc_attr($more_text); ?>">
+        </p>
+        <p>
+            <label for="<?php echo $this->get_field_id('more_link'); ?>"><?php _e('More dataset: Link (URL)', 'wpckan'); ?></label>
+            <input class="widefat" type="text" id="<?php echo $this->get_field_id('more_link'); ?>" name="<?php echo $this->get_field_name('more_link'); ?>" value="<?php echo esc_attr($more_link); ?>">
+        </p>
 		<h3>Output</h3>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'template' ); ?>"><?php _e( 'Select layout:' ); ?></label>
@@ -223,7 +238,7 @@ class Wpckan_Query_Resources_By_Topic_Widget extends WP_Widget {
 		$instance = array();
 		$instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
 		$instance['organization'] = (!empty($new_instance['organization'])) ? strip_tags($new_instance['organization']) : '';
-		$instance['search_field'] = (!empty($new_instance['search_field'])) ? strip_tags($new_instance['search_field']) : 'title';
+		$instance['search_field'] = (!empty($new_instance['search_field'])) ? strip_tags($new_instance['search_field']) : 'taxonomy';
 		$instance['limit'] = (!empty($new_instance['limit'])) ? strip_tags($new_instance['limit']) : -1;
 		$instance['type'] = (! empty( $new_instance['type'])) ? strip_tags( $new_instance['type'] ) : 'dataset';
 		$instance['output_fields'] = (!empty($new_instance['output_fields'])) ? strip_tags($new_instance['output_fields']) : 'title';
@@ -232,6 +247,8 @@ class Wpckan_Query_Resources_By_Topic_Widget extends WP_Widget {
 		$instance['output_fields_resources'] = wpckan_remove_whitespaces($instance['output_fields_resources']);
 		$instance['sort'] = (! empty( $new_instance['sort'])) ? strip_tags( $new_instance['sort'] ) : 'metadata_modified+desc';
 		$instance['template'] = (!empty( $new_instance['template'])) ? $new_instance['template'] : 'dataset-list';
+		$instance['more_text'] = (!empty($new_instance['more_text'])) ? $new_instance['more_text'] : 0;
+        $instance['more_link'] = (!empty($new_instance['more_link'])) ? $new_instance['more_link'] : 0;
 
 		return $instance;
 	}
@@ -239,5 +256,3 @@ class Wpckan_Query_Resources_By_Topic_Widget extends WP_Widget {
 }
 
 add_action( 'widgets_init', function(){register_widget("Wpckan_Query_Resources_By_Topic_Widget");});
-
-?>
